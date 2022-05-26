167be4c55ece22f7ad9bae979db77885c7db1f16
CharleVince <charlotvncnt@gmail.com>
2022-05-26 18:12:17 +0300

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,33 @@
+const request = require("postman-request");
+
+const forecast = (longitude, latitude, callback) => {
+  const url =
+    "http://api.weatherstack.com/current?access_key=3bba2d08bd60b6daa4b345980838fc2e&query=" +
+    latitude +
+    "," +
+    longitude +
+    "&units=f";
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connnect weather service", undefined);
+    } else if (body.error) {
+      callback(
+        "Unable to find location. Please try another search.",
+        undefined
+      );
+    } else {
+      callback(undefined, {
+        forecast:
+          body.current.weather_descriptions[0] +
+          " It is currently " +
+          body.current.temperature +
+          " degrees out." +
+          " There is a " +
+          body.current.precip +
+          "% chance of rain.",
+      });
+    }
+  });
+};
+
+module.exports = forecast;
